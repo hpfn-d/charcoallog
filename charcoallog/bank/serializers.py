2650532fcf2f422bfd0a58abbb7892553deef0ad
hpfn-d <terberh@gmail.com>
2019-04-08 16:08:22 -0300

    Schedule Api, show/hide, update value
    close #92

--- charcoallog/bank/serializers.py
+++ charcoallog/bank/serializers.py
@@ -1,11 +1,30 @@
 from rest_framework import serializers
 
-from charcoallog.bank.models import Extract
+from charcoallog.bank.models import Extract, Schedule
 
 
 class ExtractSerializer(serializers.ModelSerializer):
     class Meta:
         model = Extract
+        fields = ('payment', 'category', 'description', 'money', 'date', 'pk')
+
+    def update(self, instance, validated_data):
+        instance.pk = validated_data.get('pk', instance.pk)
+        instance.date = validated_data.get('date', instance.date)
+        instance.money = validated_data.get('money', instance.money)
+        instance.description = validated_data.get('description', instance.description)
+        instance.category = validated_data.get('category', instance.category)
+        instance.payment = validated_data.get('payment', instance.payment)
+        instance.user_name = validated_data.get('user_name', instance.user_name)
+
+        instance.save()
+
+        return instance
+
+
+class ScheduleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Schedule
         fields = ('payment', 'category', 'description', 'money', 'date', 'pk')
 
     def update(self, instance, validated_data):
--- charcoallog/bank/urls.py
+++ charcoallog/bank/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from charcoallog.bank.views import HomeApi, home
+from charcoallog.bank.views import HomeApi, ScheduleApi, home
 
 app_name = 'bank'
 
 urlpatterns = [
     path('', home, name='home'),
     path('home_api/<int:pk>/', HomeApi.as_view(), name='home_api'),
+    path('schedule_api/<int:pk>/', ScheduleApi.as_view(), name='schedule_api'),
 ]
 #   path('update/', update, name='update'),
 #   path('delete/', delete, name='delete'),
