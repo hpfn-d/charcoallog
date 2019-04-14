5117acb1633029040584c282c4210ede393ec0c9
hpfn-d <terberh@gmail.com>
2019-04-14 11:54:30 -0300

    core template cleaning
    close #104

--- charcoallog/core/views.py
+++ charcoallog/core/views.py
@@ -6,5 +6,10 @@ from charcoallog.core.service import BuildHome
 
 @login_required
 def home(request):
-    context = {'build_home': BuildHome(request.user)}
+    b_h = BuildHome(request.user)
+    context = {'names': b_h.line1.account_names(),
+               'wht_l': b_h.line1.whats_left,
+               'brokerage': b_h.line2.brokerage,
+               'investdetail': b_h.line2.kind_investmentdetail()}
+
     return render(request, "core/home.html", context)
