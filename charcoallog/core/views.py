c37c75992ba2687ffbc3139088e4a8792f7d923e
hpfn-d <terberh@gmail.com>
2019-06-05 18:27:06 -0300

    core shows summary
    close #146

--- charcoallog/core/views.py
+++ charcoallog/core/views.py
@@ -10,6 +10,7 @@ def home(request):
     context = {'names': b_h.line1.account_val_sorted(),
                'wht_l': b_h.line1.whats_left,
                'brokerage': b_h.line2.brokerage,
-               'investdetail': b_h.line2.kind_investmentdetail()}
+               'investdetail': b_h.line2.kind_investmentdetail(),
+               'summary': b_h.summary_categories}
 
     return render(request, "core/home.html", context)
