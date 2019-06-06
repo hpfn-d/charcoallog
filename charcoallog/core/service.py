5b8a7f834054523dd25f2d39fb8e1debefbf4dc4
hpfn-d <terberh@gmail.com>
2019-06-06 11:02:43 -0300

    core: summary - split, tests, task
    close #149

--- charcoallog/core/service.py
+++ charcoallog/core/service.py
@@ -1,9 +1,11 @@
 import datetime as dt
+import json
+import os
 
 from charcoallog.bank.brief_bank_service import BriefBank
 from charcoallog.bank.models import Extract
 from charcoallog.bank.service import Summary
-from charcoallog.core.scrap_line3_service import Scrap
+# from charcoallog.core.scrap_line3_service import Scrap
 from charcoallog.investments.brief_investment_service import BriefInvestment
 from charcoallog.investments.models import NewInvestment, NewInvestmentDetails
 
@@ -12,28 +14,68 @@ class BuildHome:
     def __init__(self, request_user):
         self.query_bank = Extract.objects.user_logged(request_user)
         self.line1 = BriefBank(self.query_bank)
-        tabela = Scrap()
-        self.selic_info = tabela.selic_info()
-        self.ibov_info = tabela.ibov_info()
-        self.ipca_info = tabela.ipca_info()
+        # tabela = Scrap()
+        # self.selic_info = tabela.selic_info()
+        # self.ibov_info = tabela.ibov_info()
+        # self.ipca_info = tabela.ipca_info()
 
         self.query_user_invest = NewInvestment.objects.user_logged(request_user)
         self.query_user_details = NewInvestmentDetails.objects.user_logged(request_user)
         self.line2 = BriefInvestment(self.query_user_invest, self.query_user_details)
 
-        self.summary_categories = self.by_month_cat()
+        self.summary_categories = by_month_cat(request_user)
 
-    def by_month_cat(self):
-        summary = Summary(self.query_bank)
-        all_year_month = []
-        range_month = int(summary.month) + 1
-        for m in range(1, range_month):
-            month = dt.datetime(int(summary.year), m, 1).strftime("%m")
 
-            last = self.query_bank.summary('2019', month)
-            summary.month_summary = last
-            all_year_month.append(summary.summary_categories)
+SUMMARY = './charcoallog/core/summary.json'
 
-        # json.dumps to a file
-        #  do like Scrap class for ibov, selic, ipca
-        return reversed(all_year_month)
+
+def by_month_cat(request_user):
+    if os.path.isfile(SUMMARY):
+        with open(SUMMARY, 'r') as fd:
+            user_year_month = json.load(fd)
+    else:
+        user_year_month = collect_summary()
+
+    year = dt.datetime.today().strftime("%Y")
+    return user_year_month[year][str(request_user)]
+
+
+def collect_summary():
+    year = dt.datetime.today().strftime("%Y")
+
+    to_json = {year: dict()}
+
+    for who in all_users():
+        all_year_month, year = year_summary(who)
+        to_json[year].update({who: all_year_month})
+
+    with open(SUMMARY, 'w') as fd:
+        json.dump(to_json, fd)
+
+    return to_json
+
+
+# collect_summary helpers
+def all_users():
+    all_u = Extract.objects.all().values_list('user_name', flat=True)
+    return set(all_u)
+
+
+def year_summary(who):
+    query_bank = Extract.objects.user_logged(who)
+    summary = Summary(query_bank)
+    all_year_month = []
+    range_month = int(summary.month) - 1
+
+    for m in range(range_month, 0, -1):
+        wrap_year_month(query_bank, all_year_month, summary, m)
+
+    return all_year_month, summary.year
+
+
+def wrap_year_month(query_bank, all_year_month, summary, m):
+    month = dt.datetime(int(summary.year), m, 1).strftime("%m")
+    last = query_bank.summary(summary.year, month)
+    if last:
+        summary.month_summary = last
+        all_year_month.append(summary.summary_categories)
--- charcoallog/core/tests/test_service.py
+++ charcoallog/core/tests/test_service.py
@@ -1,17 +1,20 @@
+import datetime as dt
+import os
 from unittest.mock import patch
 
 from django.db.models import QuerySet
 from django.test import TestCase
 
 from charcoallog.bank.brief_bank_service import BriefBank
+from charcoallog.bank.models import Extract
 from charcoallog.core import service
-from charcoallog.core.service import BuildHome
+from charcoallog.core.service import SUMMARY, BuildHome, collect_summary
 from charcoallog.investments.brief_investment_service import BriefInvestment
 
 
 class BuildHomeTest(TestCase):
-    @patch('charcoallog.core.service.Scrap')
-    def setUp(self, mocked):
+    @patch('charcoallog.core.service.by_month_cat')
+    def setUp(self, quiet):
         self.user = 'teste'
         self.obj = BuildHome(self.user)
 
@@ -19,15 +22,16 @@ class BuildHomeTest(TestCase):
         expected = [
             hasattr(service, 'Extract'),
             hasattr(service, 'BriefBank'),
-            hasattr(service, 'Scrap'),
+            # hasattr(service, 'Scrap'),
             hasattr(self.obj, 'query_bank'),
             hasattr(self.obj, 'line1'),
-            hasattr(self.obj, 'selic_info'),
-            hasattr(self.obj, 'ibov_info'),
-            hasattr(self.obj, 'ipca_info'),
+            # hasattr(self.obj, 'selic_info'),
+            # hasattr(self.obj, 'ibov_info'),
+            # hasattr(self.obj, 'ipca_info'),
             hasattr(self.obj, 'query_user_invest'),
             hasattr(self.obj, 'query_user_details'),
             hasattr(self.obj, 'line2'),
+            hasattr(self.obj, 'summary_categories')
         ]
 
         for e in expected:
@@ -50,3 +54,71 @@ class BuildHomeTest(TestCase):
         for e in expected:
             with self.subTest():
                 self.assertTrue(e)
+
+
+class CoreSummary(TestCase):
+    def setUp(self):
+        """
+        dict[year][user]
+
+        teste1 - 2018, one record
+        teste1 - 2019, one record
+
+        teste2 - 2018, two records
+        teste2 - 2019, two records
+
+        """
+        self.now = dt.datetime.today()
+        current = self.now.strftime("%Y-%m-%d")
+
+        self.user1 = 'teste1'
+        d = '2018-01-01'
+
+        data = dict(
+            user_name=self.user1,
+            date=d,
+            money='10.00',
+            description='test',
+            category='category',
+            payment='principal'
+        )
+        Extract.objects.create(**data)
+        data['date'] = current
+        Extract.objects.create(**data)
+
+        self.user2 = 'teste2'
+
+        data['user_name'] = self.user2
+        data['date'] = current
+        Extract.objects.create(**data)
+
+        self.to_json = collect_summary()
+
+    def test_year_summary(self):
+        """ check first key - year """
+        self.assertIn(str(self.now.year), list(self.to_json.keys()))
+
+    def test_users_summary(self):
+        """ two users """
+        # check second key - user
+        self.assertEqual(len(self.to_json[str(self.now.year)]), 2)
+        self.assertIn(self.user1, self.to_json[str(self.now.year)].keys())
+        self.assertIn(self.user2, self.to_json[str(self.now.year)].keys())
+
+    def test_user_instance_value(self):
+        """ should be a list """
+        self.assertIsInstance(self.to_json[str(self.now.year)][self.user1], list)
+        self.assertIsInstance(self.to_json[str(self.now.year)][self.user2], list)
+
+    def test_content_summary(self):
+        """ core does not show current month"""
+        self.assertEqual(len(self.to_json[str(self.now.year)][self.user1]), 0)
+        self.assertEqual(len(self.to_json[str(self.now.year)][self.user2]), 0)
+
+    def test_count_records(self):
+        """ but user1 have 2 records"""
+        qs = Extract.objects.user_logged(self.user1).all()
+        self.assertEqual(qs.count(), 2)
+
+    def tearDown(self):
+        os.remove(SUMMARY)
