e927291b347a86036237706cecd90fdc50a1594e
Herbert Parentes Fortes Neto <hpfn@debian.org>
2019-08-26 18:54:42 -0300

    bank: tests_view_update/delete better now
    close #196
    
    with fixture

--- charcoallog/bank/tests/test_view_delete.py
+++ charcoallog/bank/tests/test_view_delete.py
@@ -7,62 +7,38 @@ from charcoallog.bank.models import Extract
 
 
 class AjaxPostTest(TestCase):
-    fixtures = ['user.json']
+    fixtures = ['user.json', 'update_del.json']
 
     def setUp(self):
-        self.user_name = 'test'
-        self.data = dict(
-            user_name=self.user_name,
-            date='2017-12-21',
-            money='10.00',
-            description='test',
-            category='test',
-            payment='principal'
-        )
-        Extract.objects.create(**self.data)
-
-        # keep tihs one
-        update_test = dict(
-            user_name=self.user_name,
-            date='2017-12-21',
-            money='10.00',
-            description='test',
-            category='test',
-            payment='cartao credito'
-        )
-        Extract.objects.create(**update_test)
-
-        self.login_in = self.client.login(username=self.user_name, password='1qa2ws3ed')
+        self.login_in = self.client.login(username='test', password='1qa2ws3ed')
 
     def test_login_ok(self):
         self.assertTrue(self.login_in)
 
     def test_count_records(self):
-        self.assertEqual(2, Extract.objects.filter(user_name=self.user_name).count())
+        self.assertEqual(2, Extract.objects.all().count())
 
     def test_ajax_remove(self):
         """
         Delete first record - payment == principal
-        4 asserts
         Return empty content - 204
         Only one record now
-        No payment principal
-        Check response content
+        payment == cartao
         """
-        obj = Extract.objects.get(**self.data)
+        # obj = Extract.objects.get(**self.data)
 
-        self.data['pk'] = obj.pk
-        response = self.client.delete(r('bank:home_api', obj.pk))
+        # self.data['pk'] = obj.pk
+        response = self.client.delete(r('bank:home_api', 1))
 
-        keep = Extract.objects.filter(user_name=self.user_name).count()
-        no_principal = Extract.objects.filter(payment='principal').count()
-        no_return = response.content.decode()
+        count = Extract.objects.all().count()
+        cartao = Extract.objects.filter(pk=2).exists()
+        # no_return = response.content.decode()
 
         expected = [
             (response.status_code, 204),
-            (1, keep),
-            (0, no_principal),
-            (no_return, '')
+            (1, count),
+            (True, cartao),
+            # (no_return, '')
         ]
 
         for resp, e in expected:
